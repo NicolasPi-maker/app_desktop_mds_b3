66e28e1bb9a18fcedde6ed42d2951ac796cd4ad0
mponsramells <mponsramells@gmail.com>
2024-01-30 16:35:37 +0100

    First commit

---
+++ src/MAUI_Score/MAUI_Score/View/Player.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace MAUI_Score
+{
+    public partial class Player : ContentPage
+    {
+        int count = 0;
+
+        public Player()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+
+}
