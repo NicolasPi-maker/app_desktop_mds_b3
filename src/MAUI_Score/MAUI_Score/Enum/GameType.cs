64904f7f40e0ee344a84381bb6b3ace497f46492
Pinier-Nicolas <pinier74nicolas@gmail.com>
2024-03-25 10:19:08 +0100

    Feature: create model classes

---
+++ src/MAUI_Score/MAUI_Score/Enum/GameType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAUI_Score.Enum
+{
+    public enum GameType
+    {
+        [StringSyntax("FPS")]
+        FPS,
+
+        [Display(Name = "Moba")]
+        Moba,
+
+        [Display(Name = "RTS")]
+        RTS,
+    }
+}
---
+++ src/MAUI_Score/MAUI_Score/Models/Player.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAUI_Score.Models
+{
+    public class Player
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        private string pseudo { get; set; }
+        public Team team { get; set; }
+        public float winrate { get; set; }
+        public int nbGamesPlayed { get; set; }
+
+        public Player(int id, string name, string pseudo, Team team, float winrate, int nbGamesPlayed)
+        {
+            this.id = id;
+            this.name = name;
+            this.pseudo = pseudo;
+            this.team = team;
+            this.winrate = winrate;
+            this.nbGamesPlayed = nbGamesPlayed;
+        }
+
+        public Player createPlayer(int id, string name, string pseudo, Team team, float winrate, int nbGamesPlayed)
+        {
+            return new Player(id, name, pseudo, team, winrate, nbGamesPlayed);
+        }
+    }
+}
