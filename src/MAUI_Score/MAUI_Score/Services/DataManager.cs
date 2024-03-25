b807e939e92a8744d4ebdbfd59bd4b003855d380
Pinier-Nicolas <pinier74nicolas@gmail.com>
2024-03-25 19:54:49 +0100

    create model service classes

--- src/MAUI_Score/MAUI_Score/Services/DataManager.cs
+++ src/MAUI_Score/MAUI_Score/Services/DataManager.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿using MAUI_Score.Interfaces;
+using MAUI_Score.Models;
+using MAUI_Score.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MAUI_Score.Datas
+namespace MAUI_Score.Services
 {
-    public class DataManager<T> : Interfaces.DataManagerInterface<T> where T : class
+    public class DataManager<T> : DataManagerInterface<T> where T : class
     {
         private static readonly List<T> _items = new List<T>();
         private static int _nextId = 1;
@@ -54,6 +57,28 @@ namespace MAUI_Score.Datas
                 return true;
             }
             return false;
+        }
+    }
+}
+
+public class UserService
+{
+    private readonly DataManagerInterface<Game> _gameRepository;
+
+    public UserService(DataManagerInterface<Game> gameRepository)
+    {
+        _gameRepository = gameRepository;
+    }
+
+    public void Add(Game game)
+    {
+        if (game == null)
+        {
+            throw new ArgumentNullException(nameof(game));
+        }
+        else
+        {
+            _gameRepository.Add(game);
         }
     }
 }
---
+++ src/MAUI_Score/MAUI_Score/Services/PlayerService.cs
@@ -0,0 +1,19 @@
+﻿using MAUI_Score.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAUI_Score.Services
+{
+    public class PlayerService
+    {
+        private readonly DataManagerInterface<Models.Player> _PlayerRepository;
+
+        public PlayerService(DataManagerInterface<Models.Player> PlayerRepository)
+        {
+            _PlayerRepository = PlayerRepository;
+        }
+    }
+}
